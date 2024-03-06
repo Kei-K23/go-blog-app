f7eaa394d7954d0ef00bfed17497d7776a00920f
Kei-K23 <keiksl2301@gmail.com>
2024-03-06 17:41:10 +0630

    create db connection

--- cmd/main.go
+++ cmd/main.go
@@ -1,18 +1,37 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/Kei-K23/go-blog-app/handlers"
 	"github.com/a-h/templ"
+	_ "github.com/tursodatabase/libsql-client-go/libsql"
 )
 
-func main()  {
+var db *sql.DB
+
+func init() {
+	var err error
+	url := "libsql://go-blog-app-kei-k23.turso.io?authToken=eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3MDk3MjI4MzEsImlkIjoiZmQ0MmYxMTgtNjQ0Mi00ZWZlLWEwODMtZmU5ODg5OTUyMTRkIn0.1N0st4ziWW-w9EJdyktWX1sLgXQ-mCk2g2f3zYeE5aLmb8lveHusvWJPaVC1t4trwBq4vf-Lp65s-S42YEoSBA"
+
+	db, err = sql.Open("libsql", url)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to open db %s: %s", url, err)
+		os.Exit(1)
+	}
+	defer db.Close()
+	fmt.Println("Successfully connected to database")
+}
+
+func main() {
 	mux := http.NewServeMux()
 
- 	baseHandler := handlers.BaseHandler{}	
- 	blogHandler := handlers.BlogHandler{}	
+	baseHandler := handlers.BaseHandler{}
+	blogHandler := handlers.BlogHandler{}
 
 	mux.Handle("GET /", templ.Handler(baseHandler.ShowHome()))
 	mux.Handle("GET /create", templ.Handler(blogHandler.ShowCreate()))
--- go.mod
+++ go.mod
@@ -2,4 +2,15 @@ module github.com/Kei-K23/go-blog-app
 
 go 1.22.0
 
-require github.com/a-h/templ v0.2.598
+require (
+	github.com/a-h/templ v0.2.598
+	github.com/tursodatabase/libsql-client-go v0.0.0-20240220085343-4ae0eb9d0898
+)
+
+require (
+	github.com/antlr/antlr4/runtime/Go/antlr/v4 v4.0.0-20230512164433-5d1fd1a340c9 // indirect
+	github.com/klauspost/compress v1.15.15 // indirect
+	github.com/libsql/sqlite-antlr4-parser v0.0.0-20230802215326-5cb5bb604475 // indirect
+	golang.org/x/exp v0.0.0-20220722155223-a9213eeb770e // indirect
+	nhooyr.io/websocket v1.8.7 // indirect
+)
